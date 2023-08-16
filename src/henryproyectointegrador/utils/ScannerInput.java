e0ca34b532ef19c9e56217ccf0c46c508005ea8c
jruizsilva <ruizsilvajonathan@gmail.com>
2023-08-16 10:24:12 -0300

    solicitarFecha method added

--- src/henryproyectointegrador/utils/ScannerInput.java
+++ src/henryproyectointegrador/utils/ScannerInput.java
@@ -1,9 +1,14 @@
 package henryproyectointegrador.utils;
 
+import henryproyectointegrador.excepciones.FechaIngresadaInvalidaException;
 import henryproyectointegrador.excepciones.MontoIngresadoInvalidoException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
+import static henryproyectointegrador.utils.ScannerValidator.isValidDate;
 import static henryproyectointegrador.utils.ScannerValidator.isValidDouble;
 
 public class ScannerInput {
@@ -23,6 +28,24 @@ public class ScannerInput {
             }
         } while (!isValidInput);
         return entradaDouble;
+    }
+
+    public static Date solicitarFecha(String textToShow) {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date entradaDate = null;
+        boolean isValidInput = false;
+        do {
+            System.out.println(textToShow);
+            String entrada = scanner.next();
+            try {
+                isValidInput = isValidDate(entrada, format);
+                entradaDate = format.parse(entrada);
+            } catch (FechaIngresadaInvalidaException | ParseException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!isValidInput);
+
+        return entradaDate;
     }
 
     public static String solicitarString() {
