e6c2f72ccd3d171f430d0531ee7309449649a841
jruizsilva <ruizsilvajonathan@gmail.com>
2023-08-19 22:05:00 -0300

    refactor method call

--- src/henryproyectointegrador/test/TestSeguimientoGastos.java
+++ src/henryproyectointegrador/test/TestSeguimientoGastos.java
@@ -14,6 +14,6 @@ public class TestSeguimientoGastos {
         CategoryDaoImplH2 categoryDao = new CategoryDaoImplH2();
 
         List<CategoryDto> categories = categoryDao.findAll();
-        presentacion.print(categories, CategoryDto.class);
+        presentacion.printCategories(categories);
     }
 }
